81c8988f58f6265e98f6a1681f5161e43df908bd
UnityWolft <85596586+UnityWolft@users.noreply.github.com>
2026-03-15 16:40:21 -0600

    Delete tienda.php

--- tienda.php
+++
@@ -1,5 +0,0 @@
-<?php include("templates/header.php"); ?>
-<h2>Tienda de juegos</h2>
-<div id="juegos" class="row"></div>
-<script src="js/tienda.js"></script>
-<?php include("templates/footer.php"); ?>
