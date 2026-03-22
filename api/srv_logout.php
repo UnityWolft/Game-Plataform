175b08f2cbb008e71b73c6868ee04774d864daa2
UnityWolft <85596586+UnityWolft@users.noreply.github.com>
2026-03-21 21:48:15 -0600

    Refactor logout session handling and redirection

--- api/srv_logout.php
+++ api/srv_logout.php
@@ -1,9 +1,8 @@
 <?php
-session_start(); // Unirse a la sesión actual
-session_unset(); // Limpiar todas las variables de sesión
-session_destroy(); // Destruir la sesión por completo
+session_start(); 
+session_unset();
+session_destroy(); 
 
-// Redirigir al inicio (puedes mandarlo al login o al index)
 header("Location: ../index.html");
 exit();
 ?>
