72b0b43eb9f9f895bf4f40e91b18e02784101bd9
UnityWolft <85596586+UnityWolft@users.noreply.github.com>
2026-03-24 21:04:08 -0600

    Remove unnecessary comments in recibeTexto function

--- api/lib/recibeTexto.php
+++ api/lib/recibeTexto.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * Devuelve el texto de un parámetro enviado al
- * servidor por medio de GET, POST o cookie.
- *
- * Si el parámetro no se recibe, devuelve false.
- */
 function recibeTexto(string $parametro): false|string
 {
   return isset($_REQUEST[$parametro])
