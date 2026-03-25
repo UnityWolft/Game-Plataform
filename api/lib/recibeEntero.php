903ebe89b431feb054786052f258e689f0080854
UnityWolft <85596586+UnityWolft@users.noreply.github.com>
2026-03-24 21:03:30 -0600

    Update recibeEntero.php

--- api/lib/recibeEntero.php
+++ api/lib/recibeEntero.php
@@ -1,14 +1,6 @@
 <?php
 require_once __DIR__ . "/recibeTexto.php";
 
-/**
- * Devuelve el valor entero de un parámetro recibido en el
- * servidor por medio de GET, POST o cookie.
- *
- * Si el parámetro no se recibe, devuelve false.
- * Si se recibe una cadena vacía, devuelve null.
- * Si no se puede convertir a entero, se convierte con cast.
- */
 function recibeEntero(string $parametro): false|null|int
 {
   $valor = recibeTexto($parametro);
