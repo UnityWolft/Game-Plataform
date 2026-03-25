554a748db550c715b3bd3b15e4b4db85d04bdd29
UnityWolft <85596586+UnityWolft@users.noreply.github.com>
2026-03-24 21:03:12 -0600

    Refactor error handling in manejaErrores.php

--- api/lib/manejaErrores.php
+++ api/lib/manejaErrores.php
@@ -3,14 +3,10 @@ require_once __DIR__ . "/INTERNAL_SERVER_ERROR.php";
 require_once __DIR__ . "/ProblemDetailsException.php";
 require_once __DIR__ . "/devuelveResultadoNoJson.php";
 
-/* Hace que se lance una excepción automáticamente
- * cuando se genere un error de PHP.
- */
 set_error_handler(function ($severity, $message, $file, $line) {
   throw new ErrorException($message, 0, $severity, $file, $line);
 });
 
-/* Manejo de excepciones no atrapadas. */
 set_exception_handler(function (Throwable $excepcion) {
   if ($excepcion instanceof ProblemDetailsException) {
     devuelveProblemDetails($excepcion->problemDetails);
